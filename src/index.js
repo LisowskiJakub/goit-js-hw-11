d984f48739349b4d451fd4e0e9b30be1ad66fd51
Jakub Lisowski <lisowski.jakub@gmail.com>
2023-03-19 21:47:28 +0100

    added pagination

--- src/index.js
+++ src/index.js
@@ -1,50 +1,58 @@
 import { fetchPictures } from "./js/fetchPictures";
-import { debounce } from "lodash";
+import Notiflix from "notiflix";
 const { log } = console
-const headerInput = document.querySelector('.header__input');
+const headerForm = document.querySelector('.header__form');
 const gallery = document.querySelector('.gallery')
-const DEBOUNCE_DELAY = 500;
-
+const NotiflixOptions = { distance: '2px', cssAnimationStyle: 'from-right' }
+const loadMoreBtn = document.querySelector('.load-more');
+let pageNumber = 1;
+let searchData = ''
 const renderGallery = picturesArr => {
   const markup = picturesArr
     .map((picture) => `
-            <div class="photo-card">
-          <img src="${picture.webformatURL}" alt="${picture.tags}" loading="lazy" />
-          <div class="info">
-            <p class="info-item">
-              <b>Likes</b>${picture.likes}
-            </p>
-            <p class="info-item">
-              <b>Views</b>${picture.views}
-            </p>
-            <p class="info-item">
-              <b>Comments</b>${picture.comments}
-            </p>
-            <p class="info-item">
-              <b>Downloads</b>${picture.downloads}
-            </p>
-             </div>
-            </div>`)
+       <div class="photo-card">
+    <img src="${picture.webformatURL}" alt="${picture.tags}" loading="lazy" />
+    <div class="info">
+      <p class="info-item">
+        <span class="info-title"><b>Likes</b></span><span class="info-numbers">${picture.likes}</span>
+      </p>
+      <p class="info-item">
+        <span class="info-title"><b>Views</b></span><span class="info-numbers">${picture.views}</span>
+      </p>
+      <p class="info-item">
+        <span class="info-title"><b>Comments</b></span><span class="info-numbers">${picture.comments}</span>
+      </p>
+      <p class="info-item">
+       <span class="info-title"><b>Downloads</b></span><span class="info-numbers">${picture.downloads}</span>
+      </p>
+    </div>
+        </div>`)
     .join('')
-  gallery.innerHTML = markup;
+  gallery.insertAdjacentHTML('beforeend', markup);
 
 }
 
 
-headerInput.addEventListener('input', debounce(() => {
-
-
-  fetchPictures(headerInput.value.trim())
+headerForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  gallery.innerHTML = '';
+  const { search } = e.currentTarget;
+  searchData = search.value.trim();
+  fetchPictures(searchData, pageNumber)
     .then((data) => {
-
-      log(data)
+      if (data.hits.length === 0) return Notiflix.Notify.warning('Sorry, there are no images matching your search query. Please try again.', NotiflixOptions)
       renderGallery(data.hits);
     })
+    .catch((err) => console.log(err))
+})
 
 
-
-
-}, DEBOUNCE_DELAY))
-
-
-
+loadMoreBtn.addEventListener('click', () => {
+  pageNumber++;
+  fetchPictures(searchData, pageNumber)
+    .then((data) => {
+      if (data.hits.length === 0) return Notiflix.Notify.warning('Sorry, there are no more images matching your search query. Please try again.', NotiflixOptions)
+      renderGallery(data.hits);
+    })
+    .catch((err) => console.log(err))
+})
