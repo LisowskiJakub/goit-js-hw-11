be07260e15208f2a0356ff850dbb67154657193f
Jakub Lisowski <lisowski.jakub@gmail.com>
2023-03-27 18:28:40 +0200

    working project

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
-import { fetchPictures } from "./js/fetchPictures";
-import Notiflix from "notiflix";
+import { fetchPictures } from './js/fetchPictures';
+import Notiflix from 'notiflix';
 import debounce from 'lodash.debounce';
+import 'simplelightbox/dist/simple-lightbox.min.css'
+import simpleLightbox from 'simplelightbox';
 const { log } = console;
 const headerForm = document.querySelector('.header__form');
 const gallery = document.querySelector('.gallery');
-const NotiflixOptions = { distance: '2px', cssAnimationStyle: 'from-right' };
-// const loadMoreBtn = document.querySelector('.load-more');
-const SCROLL_MARGIN = 1000;
-const DEBOUNCE_TIME = 300;
+const NotiflixOptions = { distance: '2px', cssAnimationStyle: 'from-right', showOnlyTheLastOne: 'true' };
+const SCROLL_MARGIN = 700;
+const DEBOUNCE_TIME = 100;
 let pageNumber = 1;
 let searchData = '';
-const search = document.querySelector('.header__input')
+let lightbox = new simpleLightbox('.gallery a');
 const renderGallery = picturesArr => {
   const markup = picturesArr
     .map((picture) => `
-       <div class="photo-card">
-    <img src="${picture.webformatURL}" alt="${picture.tags}" loading="lazy" />
+<div class="photo-card">
+              <a  href="${picture.largeImageURL}"><img src="${picture.webformatURL}" alt="${picture.tags}" loading="lazy" /></a>
     <div class="info">
       <p class="info-item">
         <span class="info-title"><b>Likes</b></span><span class="info-numbers">${picture.likes}</span>
@@ -30,10 +31,10 @@ const renderGallery = picturesArr => {
        <span class="info-title"><b>Downloads</b></span><span class="info-numbers">${picture.downloads}</span>
       </p>
     </div>
-        </div>`)
+    </div> `)
     .join('');
   gallery.insertAdjacentHTML('beforeend', markup);
-
+  lightbox.refresh();
 }
 
 
@@ -45,8 +46,9 @@ headerForm.addEventListener('submit', (e) => {
   fetchPictures(searchData, pageNumber)
     .then((data) => {
       log(data)
-      if (data.hits.length === 0) return Notiflix.Notify.warning('Sorry, there are no images matching your search query. Please try again.', NotiflixOptions);
+      if (data.hits.length === 0) return Notiflix.Notify.warning('Sorry, there are no images matching to your search query.', NotiflixOptions);
       renderGallery(data.hits);
+
     })
     .catch((err) => console.log(err))
 })
@@ -58,13 +60,15 @@ window.addEventListener("scroll", debounce(() => {
 
   if (scrolled > scrollable - SCROLL_MARGIN) {
 
-    log('ponizej 500')
     pageNumber++;
     fetchPictures(searchData, pageNumber)
       .then((data) => {
         if (data.hits.length === 0) return Notiflix.Notify.warning('Sorry, there are no more images matching your search query. Please try again.', NotiflixOptions)
         renderGallery(data.hits);
+
       })
+      .then(console.log('refresh'))
+
       .catch((err) => console.log(err))
   }
 
